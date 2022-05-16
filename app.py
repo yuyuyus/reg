b7fce88ade2b7dc7b0e144e3bcb5c01a1fc578d2
yuyuyus <103293394+yuyuyus@users.noreply.github.com>
2022-05-16 21:57:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,15 +13,15 @@ st.write("""
 - three
 """)
 
-input = st.text_input("검색어를 쓰세요")
+#input = st.text_input("검색어를 쓰세요")
 
 import os
 import sys
 import urllib.request
 client_id = "6ZHRcT3n_3XFRz64iWNo"
 client_secret = "c9ZUOpIks6"
-word=input('검색어를 입력하세요')
-encText = urllib.parse.quote(word)
+#word=input('검색어를 입력하세요')
+encText = urllib.parse.quote('라면')
 url = "https://openapi.naver.com/v1/search/blog?query=" + encText # json 결과
 # url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # xml 결과
 request = urllib.request.Request(url)
@@ -31,4 +31,5 @@ response = urllib.request.urlopen(request)
 rescode = response.getcode()
 if(rescode==200):
     response_body = response.read()
-    print(response_body.decode('utf-8'))
+    #print(response_body.decode('utf-8'))
+    st.write(response_body.decode('utf-8'))
