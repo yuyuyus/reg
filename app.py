eefac65bbff464e742b5f11f5f17e8acbea29515
yuyuyus <103293394+yuyuyus@users.noreply.github.com>
2022-05-21 00:48:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ if menu == "단어장 작성하기":
 
     if st.button('작성 완료하기'):
         wordlist=[write_date, write_word, write_mean, write_example, write_memo]
-        wordlist_df = pd.dataframe(wordlist, columns=column_name)
+        wordlist_df = pd.dataframe(wordlist, columns=['날짜', '단어', '뜻', '예문', '메모'])
         st.dataframe(wordlist_df)
         #st.write(wordlist)
         st.success('작성한 내용이 저장되었습니다.')
