f1799d14c04b154ff3e1a095989775f6e25a99c1
yuyuyus <103293394+yuyuyus@users.noreply.github.com>
2022-05-20 22:45:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,3 @@ def auth():
     user_id = st.sidebar.text_input('Input Username')
     user_pw = st.sidebar.text_input('Input Password', type='password')
     
-
-def main():
-    create_layout()
-
-
-if __name__ == '__main__':
-    main()
