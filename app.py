22baee8b831be80e958e5f91b3a18ec2c0ec4ce5
yuyuyus <103293394+yuyuyus@users.noreply.github.com>
2022-05-20 22:40:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
 import streamlit as st
 
+def auth():
+    user_id = st.sidebar.text_input('Input Username')
+    user_pw = st.sidebar.text_input('Input Password', type='password')
+    
 
-st.title("나의 검색기록")
-st.write("그동안 내가 검색한 단어 목록을 보여줍니다.")
-input = st.text_input("검색어를 쓰세요")
-st.write(input)
+    if user_id == 'John1234' and  user_pw == 'password1234': # You can call this auth info from DB or somewhere safe.
+        auth_result = True
+    else:
+        auth_result = False
+
+    return auth_result
 
 
-#list=[]
-#while True:
-#  input = st.text_input("검색어를 쓰세요")
-#  if len(list)<31:
-#   list.append(input)
-#    st.write(list)
+def create_layout():
+    auth_result = auth()
+
+    if auth_result:
+        st.sidebar.success('Correct Auth Info :)')
+        st.title('Hello World')
+        st.header('Header')
+        st.subheader('Sub Header')
+    else:
+        st.sidebar.warning('Wrong Authentication !!!')
+
+
+def main():
+    create_layout()
+
+
+if __name__ == '__main__':
+    main()
