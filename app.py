fccb8ae85d4d5a88b42d5599d4158d3a65293843
yuyuyus <103293394+yuyuyus@users.noreply.github.com>
2022-05-21 03:26:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,8 @@ if menu == "단어장 작성하기":
       
       st.success('작성한 내용이 저장되었습니다.')
       
-      df=wordlist_df.append(wordlist_df2) #,ignore_index=True
+    if st.button('작성 완료하기33'):
+      df=wordlist_df.append(wordlist_df2)
       
       st.table(df)
 
