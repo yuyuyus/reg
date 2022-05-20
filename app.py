614077dbd29ee330f9fae3c3f953a0202820b02c
yuyuyus <103293394+yuyuyus@users.noreply.github.com>
2022-05-21 01:46:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ if menu == "단어장 작성하기":
       wordlist=list([[write_date, write_word, write_mean, write_example, write_memo]])
       st.write(wordlist)
       
-      wordlist_df = pd.dataframe(wordlist)
+      wordlist_df=pd.DataFrame(wordlist)
       st.dataframe(wordlist_df)
       
       st.success('작성한 내용이 저장되었습니다.')
