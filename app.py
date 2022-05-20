0e3186249d2b76cd261eaabff741afbb74a481fd
yuyuyus <103293394+yuyuyus@users.noreply.github.com>
2022-05-20 22:45:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,6 @@ def auth():
     user_pw = st.sidebar.text_input('Input Password', type='password')
     
 
-    if user_id == 'John1234' and  user_pw == 'password1234': # You can call this auth info from DB or somewhere safe.
-        auth_result = True
-    else:
-        auth_result = False
-
-    return auth_result
-
-
-
-
 def main():
     create_layout()
 
