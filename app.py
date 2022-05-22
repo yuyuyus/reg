d7fb439a5d0e60b57ede52f053f9ad11e9a3fc40
yuyuyus <103293394+yuyuyus@users.noreply.github.com>
2022-05-22 21:36:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -98,6 +98,7 @@ if option == '삭제':
     delete = st.selectbox('아래 목록에서 삭제할 단어를 선택하세요.', options=lst)
     if st.button('선택 지우기'):
       lst.remove(delete)
+      st.selectbox('아래 목록에서 삭제할 단어를 선택하세요.', options=lst)
       if len(lst) < 1:
         st.success('단어장이 텅 비었습니다.')
     if st.button('모두 지우기'):
