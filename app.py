c518149d8523574622ae2abc59711a7cbce88d72
yuyuyus <103293394+yuyuyus@users.noreply.github.com>
2022-05-22 19:35:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -78,23 +78,23 @@ def cache_lst():
     return lst
 
 lst = cache_lst()
-input = st.text_input('何か入力して下さい')
-if st.checkbox('clear'):
+input = st.text_input('추가할 단어를 써 주세요.')
+if st.checkbox('모두 지우기'):
     caching.clear_cache()
     lst = cache_lst()
 elif input:
     lst.append(input)
 
-if st.checkbox('delete'):
-    delete = st.selectbox('削除する要素を選択して下さい', options=lst)
+if st.checkbox('삭제하기'):
+    delete = st.selectbox('삭제할 단어를 선택하세요.', options=lst)
     if st.button('Delete'):
         lst.remove(delete)
         st.success(f'Delete : {delete}')
 
-if st.checkbox('change'):
-    change_from = st.selectbox('変更する要素を選択して下さい', options=lst)
+if st.checkbox('수정하기'):
+    change_from = st.selectbox('수정할 단어를 선택하세요.', options=lst)
     change_index = lst.index(change_from)
-    change_to = st.text_input('何に変更しますか')
+    change_to = st.text_input('수정하여 넣을 내용을 적어주세요.')
     if st.button('Change'):
         lst.remove(change_from)
         lst.insert(change_index, change_to)
