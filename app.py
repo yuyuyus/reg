d0ec323b644f49cb0d25636d0ea1d7410ae02c99
yuyuyus <103293394+yuyuyus@users.noreply.github.com>
2022-05-22 20:41:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -90,22 +90,22 @@ def cache_lst():
 
 lst = cache_lst()
 option = st.radio("옵션 선택하기", ("입력", "삭제", '수정'))
-  if option == '삭제':
-    delete = st.selectbox('아래 목록에서 단어를 선택하면 삭제됩니다.', options=lst)
-    lst.remove(delete)
-    if st.button('모두 지우기'):
-      del lst[:]
-    #caching.clear_cache()
-  elif option == '수정':
-    change_from = st.selectbox('수정할 단어를 선택하세요.', options=lst)
-    change_index = lst.index(change_from)
-    change_to = st.text_input('아래와 같이 수정합니다.')
-    if st.button('수정 완료하기'):
-        lst.remove(change_from)
-        lst.insert(change_index, change_to)
-  elif option == '입력':
-    input = st.text_input('추가할 단어를 써 주세요.')
-    lst.append(input)
+if option == '삭제':
+  delete = st.selectbox('아래 목록에서 단어를 선택하면 삭제됩니다.', options=lst)
+  lst.remove(delete)
+  if st.button('모두 지우기'):
+    del lst[:]
+  #caching.clear_cache()
+elif option == '수정':
+  change_from = st.selectbox('수정할 단어를 선택하세요.', options=lst)
+  change_index = lst.index(change_from)
+  change_to = st.text_input('아래와 같이 수정합니다.')
+  if st.button('수정 완료하기'):
+      lst.remove(change_from)
+      lst.insert(change_index, change_to)
+elif option == '입력':
+  input = st.text_input('추가할 단어를 써 주세요.')
+  lst.append(input)
 
 
   
