ae3b673d6e0e47498d5557d45bc1b5b1dde9fbe1
yuyuyus <103293394+yuyuyus@users.noreply.github.com>
2022-05-22 17:29:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ column_name = ['날짜', '단어', '뜻', '예문', '메모']
 data_all = []
 
 
+
 if menu == "단어장 작성하기":
     write_date = st.date_input("작성 날짜", datetime.datetime.now())
     write_word = st.text_input("단어 입력하는 곳", placeholder="필수 입력")
@@ -31,17 +32,22 @@ if menu == "단어장 작성하기":
        write_memo = st.text_input("메모 입력하는 곳")
     else: 
        write_memo = '메모 미입력'
-  
 
-    for i in [1,2,3]: 
-      if st.button('작성 완료하기'):
-        wordlist=list([[write_date, write_word, write_mean, write_example, write_memo]])
-        globals()['wordlist_df_{}'.format(i)]=pd.DataFrame(wordlist, columns=column_name)
-        st.table(wordlist_df_i)
-    
-      #wordlist_df_i=wordlist_df1.append(wordlist_df2, ignore_index =True)
-        st.success('작성한 내용이 저장되었습니다.')
-     
+        
+    if st.button('작성 완료하기'):
+      wordlist1=list([[write_date, write_word, write_mean, write_example, write_memo]])
+      wordlist2=list([[write_date, write_word, write_mean, write_example, write_memo]])
+
+      wordlist_df1=pd.DataFrame(wordlist1, columns=column_name)
+      wordlist_df2=pd.DataFrame(wordlist2, columns=column_name)
+      
+
+      st.table(wordlist_df1)  #st.dataframe(wordlist_df)
+      st.table(wordlist_df2)
+      
+      wordlist_df1=wordlist_df1.append(wordlist_df2, ignore_index =True)
+      st.table(wordlist_df1)
+      st.success('작성한 내용이 저장되었습니다.')
       
       
       
