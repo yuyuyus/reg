491f8b36edf2194e33da0dac90f9f61fa9ad22b1
yuyuyus <103293394+yuyuyus@users.noreply.github.com>
2022-05-22 19:49:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -71,6 +71,27 @@ option = st.sidebar.selectbox("단어장 조회 방법을 선택하세요.",
 
 if option == "날짜":
     today = st.sidebar.date_input("날짜를 선택하세요.", datetime.datetime.now())
+
+    
+text = st.text_input("表示したい単語を入力してください")
+
+if 'text_list' not in st.session_state:
+  st.session_state["text_list"] = []
+
+col1, col2 = st.columns(2)
+
+with col1:
+  if st.button("追加", key=2):
+    st.session_state["text_list"].append(text)
+
+with col2:
+  if st.button("削除", key=3): 
+    st.session_state["text_list"].remove(text)
+      
+for output_text in st.session_state["text_list"]:
+  st.write("", output_text)    
+    
+    
     
 @st.cache(allow_output_mutation=True)
 def cache_lst():
