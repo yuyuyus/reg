82e1ebb7e907ef2d71090d5c4e632497556f7428
yuyuyus <103293394+yuyuyus@users.noreply.github.com>
2022-05-22 23:42:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ for output_text in st.session_state["text_list"]:
     
 @st.cache(allow_output_mutation=True)
 def cache_lst():
-    lst = {}
+    lst = []
     return lst
   
 
@@ -98,6 +98,7 @@ if option == '삭제':
     delete = st.multiselect('아래 목록에서 삭제할 단어를 선택하세요.', options=lst)
     if st.button('선택 지우기'):
       lst.extend(delete)
+      lst=set(lst)
     if st.button('모두 지우기'):
       del lst[:]
       st.success('단어장이 텅 비었습니다.')
@@ -118,11 +119,12 @@ elif option == '입력':
     st.button('입력하기')
   else : 
     if st.button('입력하기'):
-      lst.update(input)
+      lst.append(input)
+      lst=set(lst)
 
 
   
     
 
-st.table(lst)
+st.table([lst])
 
