13fe8d1acf2904dd2621a419b44c2599b06cdb58
yuyuyus <103293394+yuyuyus@users.noreply.github.com>
2022-05-23 10:19:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,61 @@
 import streamlit as st
 import datetime
 import pandas as pd
+
+st.title('나의 단어장 작성하기')
+
+@st.cache(allow_output_mutation=True)
+
+def cache_lst():
+    lst = []
+    return lst
+'''
+if 'lst' not in st.session_state:
+  st.session_state.lst = []
+'''
+  
+lst = cache_lst()
+option = st.radio("옵션 선택하기", ("입력", "삭제", '수정'))
+
+if option == '삭제':
+  if len(lst) < 1:
+    st.success('삭제할 단어가 존재하지 않습니다.')
+  else:
+    delete = st.multiselect('아래 목록에서 삭제할 단어를 선택하세요.', options=lst)
+    if st.button('선택 지우기'):
+      for i in delete:
+        if i in lst:
+          lst.remove(i)
+          
+      
+    if st.button('모두 지우기'):
+      del lst[:]
+      st.success('단어장이 텅 비었습니다.')
+      
+elif option == '수정':
+  if len(lst) < 1:
+    st.success('수정할 단어가 존재하지 않습니다.')
+  else:
+    change_from = st.selectbox('수정할 단어를 선택하세요.', options=lst)
+    change_index = lst.index(change_from)
+    change_to = st.text_input('아래와 같이 수정합니다.')
+    if st.button('수정 완료하기'):
+      lst.remove(change_from)
+      lst.insert(change_index, change_to)
+      
+elif option == '입력':
+  input = st.text_input('추가할 단어를 써 주세요.')
+  if input == "":
+    st.success('입력할 내용을 적고 입력 버튼을 눌러주세요.')
+    st.button('입력하기')
+  else : 
+    if st.button('입력하기'):
+      lst.append(input)
+      
+
+st.table(lst)
+
+
 '''
 @st.cache(allow_output_mutation=True)
 st.sidebar.title('Menu')
@@ -82,57 +137,3 @@ for output_text in st.session_state["text_list"]:
   st.write("", output_text)    
 '''
     
-@st.cache(allow_output_mutation=True)
-
-def cache_lst():
-    lst = []
-    return lst
-'''
-if 'lst' not in st.session_state:
-  st.session_state.lst = []
-'''
-  
-lst = cache_lst()
-option = st.radio("옵션 선택하기", ("입력", "삭제", '수정'))
-
-if option == '삭제':
-  if len(lst) < 1:
-    st.success('삭제할 단어가 존재하지 않습니다.')
-  else:
-    delete = st.multiselect('아래 목록에서 삭제할 단어를 선택하세요.', options=lst)
-    if st.button('선택 지우기'):
-      for i in delete:
-        if i in lst:
-          lst.remove(i)
-          
-      
-    if st.button('모두 지우기'):
-      del lst[:]
-      st.success('단어장이 텅 비었습니다.')
-      
-elif option == '수정':
-  if len(lst) < 1:
-    st.success('수정할 단어가 존재하지 않습니다.')
-  else:
-    change_from = st.selectbox('수정할 단어를 선택하세요.', options=lst)
-    change_index = lst.index(change_from)
-    change_to = st.text_input('아래와 같이 수정합니다.')
-    if st.button('수정 완료하기'):
-      lst.remove(change_from)
-      lst.insert(change_index, change_to)
-      
-elif option == '입력':
-  input = st.text_input('추가할 단어를 써 주세요.')
-  if input == "":
-    st.success('입력할 내용을 적고 입력 버튼을 눌러주세요.')
-    st.button('입력하기')
-  else : 
-    if st.button('입력하기'):
-      lst.append(input)
-      
-
-  
-    
-
-st.table(lst)
-
